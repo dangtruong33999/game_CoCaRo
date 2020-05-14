5f5de8c60155a0a076e8d721ab13985419946579
vietsaclo <vietsaclo@gmail.com>
2020-05-14 10:02:37 +0700

    viet saclo da hoan thanh giao dien roi nhe

--- game_Caro/Form1.Designer.cs
+++ game_Caro/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnBanCo = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnBanCo
+            // 
+            this.pnBanCo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnBanCo.Location = new System.Drawing.Point(13, 13);
+            this.pnBanCo.Name = "pnBanCo";
+            this.pnBanCo.Size = new System.Drawing.Size(555, 344);
+            this.pnBanCo.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 369);
+            this.Controls.Add(this.pnBanCo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnBanCo;
     }
 }
 
